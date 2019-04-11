15648d1367857f9e179a1673da7c88cedb499275
rjustin <ryan.justin@ymail.com>
2019-04-11 12:43:01 +0800

    Refactor using class and added async await in fetch

--- js/script.js
+++ js/script.js
@@ -1,96 +1,91 @@
+const URL = 'https://api.discountapi.com/v2/deals';
 const API_KEY = 'wGGxakRQ';
-const url = 'https://api.discountapi.com/v2/deals';
 
 const btn_search = document.querySelector('#btn-search');
 const search_text_box = document.querySelector('#search-text-box');
 const main = document.querySelector('#main');
 const loader = document.querySelector('.loader');
-const filterWrapper = document.querySelector('.filterWrapper');
+const filter_wrapper = document.querySelector('.filter-wrapper');
 const container_data = document.querySelector('#container-data');
-const wrapper = document.querySelector('.wrapper');
+const table_wrapper = document.querySelector('.table-wrapper');
 const btn_filter = document.querySelectorAll('.btn-filter');
 
-const createElement = (element) => {
-    return document.createElement(element);
-};
+let products = [];
 
-const append = (parent, child) => {
-    return parent.appendChild(child);
-};
-
-let deals = [];
-
-const renderPage = (filteredBy) => {
-    container_data.innerHTML = '';
-    let new_deals = deals;
-
-    if(filteredBy) {
-        new_deals = deals.filter((deal) => deal.deal.discount_percentage > filteredBy);
+class Product {
+    constructor(title, description, discount, fine_print) {
+        this.title = title;
+        this.description = description;
+        this.discount = discount * 100 + '%';
+        this.fine_print = fine_print;
     }
 
+    generateHTML() {
+        return `<td>${this.title}</td><td>${this.description}</td><td>${this.discount}</td><td>${this.fine_print}</td>`;
+    }
+}
+
+async function getProducts (searchParam) {
+    let data = await fetch(`${URL}?api_key=${API_KEY}&query=${searchParam}`);
+    let response = await data.json();
+    products = response.deals;
+    return products;
+}
+
+const getProductsByFilter = ((filterValue) => {
+    return products.filter((product) => product.deal.discount_percentage > filterValue);
+});
+
+const createElement = ((element) => {
+    return document.createElement(element);
+});
+
+const append = ((parent, child) => {
+    return parent.appendChild(child);
+});
+
+const loadingData = () => {
+    btn_search.disabled = true;
+    loader.classList.remove('hidden');
+    filter_wrapper.classList.add('hidden');
+    main.classList.remove('main-consumer-background');
+    container_data.innerHTML = '';
+}
+
+const dataLoaded = () => {
+    btn_search.disabled = false;
+    loader.classList.add('hidden');
+    filter_wrapper.classList.remove('hidden');
+    table_wrapper.classList.remove('hidden');
+}
+
+const renderPage = (products) => {
     let thead_el = createElement('thead');
     let tr_el = createElement('tr');
     tr_el.innerHTML = `<th class='row-name'>Shop Name</th><th class='row-desc'>Description</th><th class='row-offer'>Offer</th><th class='row-fine-print'>Fine Print</th>`
     append(thead_el, tr_el);
+    append(container_data, thead_el);
 
     let tbody_el = createElement('tbody');
     
-    new_deals.forEach((deal) => {
-        const dealDetails = deal.deal;
-
+    products.forEach((p) => {
+        const product = new Product(p.deal.title, p.deal.description, p.deal.discount_percentage, p.deal.fine_print);
         tr_el = createElement('tr');
-        
-        let td_el = createElement('td');
-        let text = document.createTextNode(dealDetails.title);
-        append(td_el, text);
-        append(tr_el, td_el);
-
-        td_el = createElement('td');
-        text = document.createTextNode(dealDetails.description);
-        append(td_el, text);
-        append(tr_el, td_el);
-
-        td_el = createElement('td');
-        let disc = dealDetails.discount_percentage * 100 + '%';
-        text = document.createTextNode(disc);
-        append(td_el, text);
-        append(tr_el, td_el);
-
-        td_el = createElement('td');
-        text = document.createTextNode(dealDetails.fine_print);
-        append(td_el, text);
-        append(tr_el, td_el);
-
+        tr_el.innerHTML = product.generateHTML();
         append(tbody_el, tr_el);
     });
 
-    append(container_data, thead_el);
     append(container_data, tbody_el);
-    wrapper.classList.remove('hidden');
 };
 
-const getProducts = (param) => {
-    return fetch(`${url}?api_key=${API_KEY}&query=${param}`)
-        .then((data) => {
-            return data.json();
-        })
-        .then((response) => {
-            deals = response.deals;
-            renderPage();
-            btn_search.disabled = false;
-            loader.classList.add('hidden');
-            filterWrapper.classList.remove('hidden');
-        });
-}
-
 btn_search.onclick = function() {
-    btn_search.disabled = true;
-    loader.classList.remove('hidden');
-    filterWrapper.classList.add('hidden');
-    main.classList.remove('main-consumer-background');
-    console.log('search_text_box_value:', search_text_box.value);
-    
-    getProducts(search_text_box.value.toLowerCase());   
+    loadingData();
+
+    getProducts(search_text_box.value.toLowerCase())
+        .then((products) => {
+            renderPage(products);
+            dataLoaded();
+        });
 }
 
 document.addEventListener('click', function(e) {
@@ -99,11 +94,17 @@ document.addEventListener('click', function(e) {
             btn.classList.remove('btn-filter-active');
         });
         e.target.classList.add('btn-filter-active');
-        renderPage(e.target.value);
+
+        const newProducts = getProductsByFilter(e.target.value);
+
+        loadingData();
+
+        renderPage(newProducts);
+        dataLoaded();
     }      
 });
 
 document.addEventListener('keypress', function (e) {
     if(e.which == 13)
         btn_search.click();
-})
+});
